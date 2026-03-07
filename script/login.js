3d278898d98f9e63561230a2256bd6f1b96aeeb3
hasan-goljar-dev <hasangolaj.r@gmail.com>
2026-03-08 01:03:57 +0600

    Login Js Logic complete

---
+++ script/login.js
@@ -0,0 +1,15 @@
+document.getElementById("btn-login")
+    .addEventListener('click', function () {
+        const  inputUsername = document.getElementById("username")
+        const  username= inputUsername.value;
+        const inputPassword = document.getElementById("password")
+        const  password= inputPassword.value;
+        if (username == "admin" && password == "admin123") {
+            alert("Login Successful");
+            window.location.assign("./index.html")
+        }
+        else {
+            alert("Login Failed");
+            return;
+        }
+    });
